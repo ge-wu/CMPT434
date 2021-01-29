bfad0482c9ed8a60b836d3005b0be0f06217a3af
ge-wu <jiayewang65@gmail.com>
2021-01-28 19:54:32 -0600

    Merge tcp server and client to network file

--- play_around/network.c
+++ play_around/network.c
@@ -1,9 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "network.h"
 
-void udp_message_sender(int sockfd, struct addrinfo *p) {
+char *weather_lst[7] = {
+    "Blizzard     ", 
+    "Comet Strike ", 
+    "Drought      ", 
+    "Comet Strike ", 
+    "Drought      ", 
+    "Flood        ", 
+    "Meteor Shower"
+};
+
+char *day[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+
+char *get_weather(char msg[]) {
+  size_t n = strlen(msg);
+  if (*msg && msg[n - 1] == '\n') {
+    msg[n - 1] = '\0';
+  }
+  for (int i = 0; i < 7; i++) {
+    if (strcmp(day[i], msg) == 0) {
+      return weather_lst[i];
+    }
+  }
+  return "Error: invalid input.";
+}
+
+void send_tcp_message(int sockfd) {
+  char buf[MSG_LEN];
+  while (1) {
+    printf("$ ");
+    scanf("%s", buf);
+    send(sockfd, buf, strlen(buf), 0);
+
+    bzero(buf, sizeof buf);
+    recv(sockfd, buf, sizeof buf, 0);
+    if ((strncmp(buf, "disconnect", 10)) == 0) {
+      printf("TCP client exit\n");
+      return;
+    }
+    printf("%s\n", buf);
+  }
+}
+
+void response_tcp_server(int sockfd) {
+  char buff[MSG_LEN]; 
+  char *response;
+
+  while (1) { 
+    bzero(buff, sizeof buff);
+    recv(sockfd, buff, sizeof buff, 0); 
+    printf("From client: %s\n", buff); 
+
+    if (strncmp("exit", buff, 4) == 0) {
+      send(sockfd, "disconnect", 10 + 1, 0);
+      printf("Server exit\n");
+      break;
+    } 
+    response = get_weather(buff);
+    send(sockfd, response, strlen(response), 0); 
+  } 
+}
+
+void send_udp_message(int sockfd, struct addrinfo *p) {
   char buf[MSG_LEN];
   int numbytes;
 
@@ -17,13 +80,20 @@ void udp_message_sender(int sockfd, struct addrinfo *p) {
     bzero(buf, sizeof buf);
     numbytes = recvfrom(sockfd, buf, MSG_LEN - 1, 0, p->ai_addr, &p->ai_addrlen);
     buf[numbytes] = '\0';
+
+    if ((strncmp(buf, "disconnect", 10)) == 0) {
+      printf("UDP talker exit\n");
+      break;
+    }
+    printf("%s\n", buf);
   }
 }
 
-void udp_message_responser(int sockfd) {
+void response_udp_message(int sockfd) {
   struct sockaddr_storage their_addr;
   socklen_t addr_len;
   char buf[MSG_LEN];
+  char *response;
   int numbytes;
 
   addr_len = sizeof their_addr;
@@ -33,15 +103,22 @@ void udp_message_responser(int sockfd) {
         perror("UDP listener: recvfrom");
         return;
     }
-    if (sendto(sockfd, "hello", 5, 0, 
+    if (strncmp("exit", buf, 4) == 0) {
+      sendto(sockfd, "disconnect", 10, 0, (struct sockaddr *)& their_addr, addr_len);
+      printf("UDP listener exit\n");
+      break;
+    }
+    response = get_weather(buf);
+    
+    if (sendto(sockfd, response, strlen(response), 0, 
       (struct sockaddr *)& their_addr, addr_len) == -1) {
         perror("UDP listener: sendto");
         exit(1);
     }
-    printf("Hello message sent.\n");
   }
 }
 
+/* UDP server */
 int udp_listener() {
   int sockfd, rv;
   struct addrinfo hints, *serverinfo, *p;
@@ -76,13 +153,13 @@ int udp_listener() {
   freeaddrinfo(serverinfo);
 
   printf("UDP listener: wainting to recvfrom...\n");
-
-  udp_message_responser(sockfd);
+  response_udp_message(sockfd);
   close(sockfd);
 
   return 0;
 }
 
+/* UDP client */
 int udp_talker(char* hostname, char* port) {
   int sockfd, rv;
   struct addrinfo hints, *serverinfo, *p;
@@ -108,10 +185,128 @@ int udp_talker(char* hostname, char* port) {
     return 2;
   }
 
-  udp_message_sender(sockfd, p);
+  send_udp_message(sockfd, p);
 
   freeaddrinfo(serverinfo);
   close(sockfd);
 
   return 0;
 }
+
+/* TCP client */
+int tcp_client(char* hostname, char* port) {
+  int sockfd, rv;
+  struct addrinfo hints, *serverinfo, *p;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((rv = getaddrinfo(hostname, port, &hints, &serverinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}	
+  // loop through all the results and connect to the first we can
+	for(p = serverinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("client: socket");
+			continue;
+		}
+
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			perror("client: connect");
+			close(sockfd);
+			continue;
+		}
+    break;
+	}
+
+  if (p == NULL) {
+    fprintf(stderr, "client: failed to connect\n");
+    return 2;
+  }  
+  freeaddrinfo(serverinfo);
+
+  printf("--------------------------------------\n"
+      "|        Weekly Weather Query        |\n"
+      "|Usage:                              |\n"
+      "|  $ <Mon|Tue|Wed|Thu|Fri|Sat|Sun>   |\n"
+      "|<all> to check the weather list     |\n"
+      "|Enter <exit> to quit                |\n"
+      "--------------------------------------\n");
+  send_tcp_message(sockfd);
+  close(sockfd);
+
+  return 0;
+}
+
+int tcp_server() {
+	int sockfd, new_fd; 
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; 
+	socklen_t sin_size;
+	int yes=1;
+	int rv;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+
+	if ((rv = getaddrinfo(NULL, TCP_PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+				sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if (p == NULL)  {
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+
+	sin_size = sizeof their_addr;
+	new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+	if (new_fd == -1) {
+    printf("server: accept failed\n");
+		exit(0);
+	} else {
+		printf("server: accepted the connection.\n");
+	}
+
+  response_tcp_server(new_fd);
+	close(new_fd);
+	return 0;
+}
+
--- play_around/network.h
+++ play_around/network.h
@@ -3,7 +3,6 @@
 
 #include <unistd.h>
 #include <errno.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -11,28 +10,17 @@
 #include <netdb.h>
 
 #define MSG_LEN 1024
+#define BACKLOG 10
+
+#define PROXY_PORT "30001"
 #define TCP_PORT "30002"
 #define UDP_PORT "30003"
 
-// char *weather_lst[7] = {
-//     "Blizzard     ", 
-//     "Comet Strike ", 
-//     "Drought      ", 
-//     "Comet Strike ", 
-//     "Drought      ", 
-//     "Flood        ", 
-//     "Meteor Shower"
-// };
-
-// char *date[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-
 int udp_talker(char*, char*);
 int udp_listener();
-void udp_message_sender(int, struct addrinfo *);
-void udp_message_responser(int);
 
-// void tcp_server();
-// void tcp_listener();
+int tcp_server();
+int tcp_client(char*, char*);
 
 #endif
 
